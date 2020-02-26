5b279ce0105f69bf591716e4bc22ecb5bd6caa9f
hqiu <hqiu@openiii.com>
2020-02-26 15:45:29 -0330

    Pascal Triangle example of 2D array added to Main

--- src/DataStructuresCourse/Main.java
+++ src/DataStructuresCourse/Main.java
@@ -2,7 +2,21 @@ package DataStructuresCourse;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        // write your code here
+    }
+
+    // Pascal Triangle example of 2D array.
+    private static int[][] pascalTriangle(int n) {
+        int[][] pt = new int[n][];
+        for (int i = 0; i < n; i++) {
+            pt[i] = new int[i + 1];
+            pt[i][0] = 1;
+            for (int j = 1; j < i; j++) {
+                pt[i][j] = pt[i - 1][j - 1] + pt[i - 1][j];
+            }
+            pt[i][i] = 1;
+        }
+        return pt;
     }
 }
